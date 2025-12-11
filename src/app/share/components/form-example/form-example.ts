eafb7d1a2df42bba649572b15eb55985b0fc88eb
Rodrigo Barbosa <rodrigo.silva018@gmail.com>
2025-12-10 21:24:02 -0400

    Adicionado folder de componentes

---
+++ src/app/share/components/form-example/form-example.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-example',
+  imports: [],
+  templateUrl: './form-example.html',
+  styleUrl: './form-example.css',
+})
+export class FormExample {
+
+}
